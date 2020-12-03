f5cdbfcaa2fe35230b690bdfdea5c0e32f6b6c75
Michelle D Zhang <zhang.michelle.d@gmail.com>
2020-12-03 10:48:23 -0600

    first pass done

--- parse.py
+++ parse.py
@@ -1,5 +1,7 @@
+import json
 import logging
 import pdb
+import pprint
 import re
 
 logging.basicConfig(level=logging.DEBUG)
@@ -11,7 +13,7 @@ with open("dbt.log", "r") as f:
 
 DBT_LOG_TEST_PASS_PATTERN = r"\d{2}:\d{2}:\d{2} \| (\d+) of \d+ PASS (\w+)(\.)* .*"
 DBT_LOG_TEST_FAIL_PATTERN = (
-    r"\d{2}:\d{2}:\d{2} \| (\d+) of \d+ FAIL (\d+) (\w+)(\.)* .*"
+    r"\d{2}:\d{2}:\d{2} \| (\d+) of \d+ (FAIL|WARN) (\d+) (\w+)(\.)* .*"
 )
 
 DBT_LOG_TEST_START_PATTERN = (
@@ -38,10 +40,10 @@ found_done = False
 metadata = {}
 all_test_metadata = {}
 finished_line_no = -1
+last_error_detail = {}
+has_incomplete_error_detail = False
 
 for line_no, line in enumerate(log_lines):
-    line = escape_ansi(line)
-
     if not found_start:
         m = re.search(DBT_LOG_START_PATTERN, line)
         if m is not None:
@@ -93,6 +95,7 @@ for line_no, line in enumerate(log_lines):
             test_metadata = {}
             test_metadata["number"] = int(m.group(1))
             test_metadata["name"] = m.group(2)
+            test_metadata["status"] = "PASS"
 
             # bash ansi escape codes are tricky to unescape if the dbt log
             # has been wrapped by another service that backslash escapes the
@@ -108,8 +111,9 @@ for line_no, line in enumerate(log_lines):
         if m is not None:
             logging.debug(f"Found a test fail line: {line}")
             test_metadata = {}
-            test_metadata["number"] = int(m.group(1))
-            test_metadata["name"] = m.group(3)
+            test_metadata["status"] = m.group(2)
+            test_metadata["number"] = int(m.group(3))
+            test_metadata["name"] = m.group(4)
 
             # same comment as above for DBT_LOG_TEST_PASS_PATTERN
             m2 = re.search(r" in (\d+\.\d+)s\]", line[line.index(m.group(0)) :])
@@ -124,23 +128,60 @@ for line_no, line in enumerate(log_lines):
             found_finish = True
             finished_line_no = line_no
             metadata["tests_run"] = int(m.group(1))
-            metadata["tests_runtime"] = float(m.group(2))
+            metadata["tests_runtime_seconds"] = float(m.group(2))
             continue
 
-        logging.debug(f"Tossing unrecognized filler line: {line}")
+        logging.debug(f"Tossing unrecognized pre-finish line: {line}")
 
+    if found_finish:
+        if has_incomplete_error_detail:
+            m = re.search(r"Got (\d+) results?, expected (\d+)", line)
+            if m is not None:
+                last_error_detail["query_results"] = {
+                    "found": int(m.group(1)),
+                    "expected": int(m.group(2)),
+                }
+                continue
 
-# special handling for error messages
+            m = re.search(r"compiled SQL at ([\w\.\/]+)\.sql", line)
+            if m is not None:
+                sql_filepath = m.group(1) + ".sql"
 
-# TODO: error message parsing
-pdb.set_trace()
+                try:
+                    with open(sql_filepath, "r") as f:
+                        sql = f.read()
+                except FileNotFoundError:
+                    sql = None
 
-m = re.search(DBT_LOG_DONE_PATTERN, line)
-if m is not None:
-    found_complete = True
-    metadata["total_passed"] = m.group(1)
-    metadata["total_errors"] = m.group(2)
-    metadata["total_warnings"] = m.group(3)
-    metadata["total_skipped"] = m.group(4)
+                last_error_detail["query"] = {
+                    "filepath": sql_filepath,
+                    "sql": sql,
+                    "file_err": True if sql is None else False,
+                }
 
-raise Exception(f"Unrecognized line after done check: {line}")
+                all_test_metadata[last_error_detail["name"]].update(last_error_detail)
+                last_error_detail = {}
+                has_incomplete_error_detail = False
+                continue
+
+            logging.debug(f"Tossing unrecognized intra error detail line: {line}")
+        else:
+            m = re.search(r"(Failure|Warning) in test (\w+)", line)
+
+            if m is not None:
+                has_incomplete_error_detail = True
+                last_error_detail["name"] = m.group(2)
+                continue
+
+            m = re.search(DBT_LOG_DONE_PATTERN, line)
+            if m is not None:
+                metadata["total_passed"] = m.group(1)
+                metadata["total_errors"] = m.group(2)
+                metadata["total_warnings"] = m.group(3)
+                metadata["total_skipped"] = m.group(4)
+                break
+
+metadata["tests"] = list(all_test_metadata.values())
+
+with open("out.json", "w") as f:
+    json.dump(metadata, f)
