3ccd867bcbb4d01658bbacae623df180ec95140e
Michelle D Zhang <zhang.michelle.d@gmail.com>
2020-12-03 14:57:19 -0600

    better cli entry

--- src/dbt_log_parser/__init__.py
+++ src/dbt_log_parser/__init__.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 
 from dbt_log_parser.parser import DbtLogParser
@@ -5,7 +6,7 @@ from dbt_log_parser.parser import DbtLogParser
 logging.basicConfig(level=logging.DEBUG)
 
 
-def main(log_filepath: str = "dbt.log"):
+def parse(log_filepath: str = "dbt.log", outfile: str = "out.json"):
     with open(log_filepath, "r") as f:
         log_lines = f.readlines()
 
@@ -17,7 +18,34 @@ def main(log_filepath: str = "dbt.log"):
 
         parser.process_next_line(line=line, line_no=line_no)
 
-    parser.report()
+    parser.report(outfile)
+
+
+def get_parser():
+    """Get a parser for pulling CLI arguments."""
+    parser = argparse.ArgumentParser(description="DBT log parser")
+
+    args = {
+        "log_filepath": dict(
+            flag="--log-filepath", help="Path to dbt log to parse", default="dbt.log"
+        ),
+        "outfile": dict(
+            flag="--outfile", help="File to write JSON results to", default="out.json"
+        ),
+    }
+
+    for arg, argspec in args.items():
+        flag = argspec.pop("flag")
+        parser.add_argument(flag, **argspec)
+
+    return parser
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
+    parse(args.log_filepath, args.outfile)
 
 
 if __name__ == "__main__":
